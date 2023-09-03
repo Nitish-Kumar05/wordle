7d9f0e2cfd2a26369285bc602c4d8721d438c385
Nitish-Kumar05 <mvnitishkumar@gmail.com>
2023-09-03 11:04:37 +0530

    added flip animation and colors to it

--- script.js
+++ script.js
@@ -125,9 +125,10 @@ const deleteLetter = () => {
 const checkRow = () => {
   const guess = guessRows[currentRow].join("");
 
-  if (currentTile === 5) {
+  if (currentTile > 4) {
     console.log(guess, wordle);
-    if (wordle === guess) {
+    flipTile();
+    if (wordle == guess) {
       giveMessage("YEP");
       isGameOver = true;
       return;
@@ -151,4 +152,43 @@ const giveMessage = (message) => {
   messageElement.textContent = message;
   messageDisplay.append(messageElement);
   setTimeout(() => messageDisplay.removeChild(messageElement), 3000);
+};
+
+// For adding color to keyboard
+const addColorToKey = (keyLetter, color) => {
+  const key = document.getElementById(keyLetter);
+  key.classList.add(color);
+};
+
+// For flipping and colors in tiles
+const flipTile = () => {
+  const rowTiles = document.querySelector("#guessRow-" + currentRow).childNodes;
+  let checkWordle = wordle;
+  const guess = [];
+
+  rowTiles.forEach((tile) => {
+    guess.push({ letter: tile.getAttribute("data"), color: "grey-overlay" });
+  });
+
+  guess.forEach((guess, index) => {
+    if (guess.letter == wordle[index]) {
+      guess.color = "green-overlay";
+      checkWordle = checkWordle.replace(guess.letter, "");
+    }
+  });
+
+  guess.forEach((guess) => {
+    if (checkWordle.includes(guess.letter)) {
+      guess.color = "yellow-overlay";
+      checkWordle = checkWordle.replace(guess.letter, "");
+    }
+  });
+
+  rowTiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add("flip");
+      tile.classList.add(guess[index].color);
+      addColorToKey(guess[index].letter, guess[index].color);
+    }, 500 * index);
+  });
 };
