faeaec83971d5a5fba57f779dbb2dbcdf3e6b71d
Nitish-Kumar05 <mvnitishkumar@gmail.com>
2023-09-03 13:13:44 +0530

    added api

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const tiles = document.querySelector(".tile-container");
+const tileDisplay = document.querySelector(".tile-container");
 const keyboard = document.querySelector(".key-container");
 const messageDisplay = document.querySelector(".message-container");
 
-const wordle = "APPLE";
+let wordle;
+
+const getWordle = () => {
+  fetch("http://localhost:8000/word")
+    .then((response) => response.json())
+    .then((json) => {
+      wordle = json.toUpperCase();
+    })
+    .catch((err) => console.log(err));
+};
+getWordle();
 
 const keys = [
   "Q",
@@ -32,9 +42,8 @@ const keys = [
   "B",
   "N",
   "M",
-  "<--",
+  "«",
 ];
-
 const guessRows = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
@@ -43,23 +52,15 @@ const guessRows = [
   ["", "", "", "", ""],
   ["", "", "", "", ""],
 ];
-
 let currentRow = 0;
 let currentTile = 0;
 let isGameOver = false;
 
-// To display tiles in the display
 guessRows.forEach((guessRow, guessRowIndex) => {
   const rowElement = document.createElement("div");
-
-  // Appending the guess row index number for easy reference
   rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
-
-  // To create 5 inside tile elements
-  guessRow.forEach((guess, guessIndex) => {
+  guessRow.forEach((_guess, guessIndex) => {
     const tileElement = document.createElement("div");
-
-    // Same appending as above for clear reference
     tileElement.setAttribute(
       "id",
       "guessRow-" + guessRowIndex + "-tile-" + guessIndex
@@ -67,10 +68,9 @@ guessRows.forEach((guessRow, guessRowIndex) => {
     tileElement.classList.add("tile");
     rowElement.append(tileElement);
   });
-  tiles.append(rowElement);
+  tileDisplay.append(rowElement);
 });
 
-// For displaying keyboard in the display
 keys.forEach((key) => {
   const buttonElement = document.createElement("button");
   buttonElement.textContent = key;
@@ -80,21 +80,19 @@ keys.forEach((key) => {
 });
 
 const handleClick = (letter) => {
-  console.log("clicked", letter);
-  if (letter === "<--") {
-    deleteLetter();
-    console.log("deleted");
-    return;
+  if (!isGameOver) {
+    if (letter === "«") {
+      deleteLetter();
+      return;
+    }
+    if (letter === "ENTER") {
+      checkRow();
+      return;
+    }
+    addLetter(letter);
   }
-  if (letter === "ENTER") {
-    checkRow();
-    console.log("checked");
-    return;
-  }
-  addLetter(letter);
 };
 
-// For adding letters in the tiles
 const addLetter = (letter) => {
   if (currentTile < 5 && currentRow < 6) {
     const tile = document.getElementById(
@@ -104,11 +102,9 @@ const addLetter = (letter) => {
     guessRows[currentRow][currentTile] = letter;
     tile.setAttribute("data", letter);
     currentTile++;
-    console.log("guessRow", guessRows);
   }
 };
 
-// For deleting letter '<--'
 const deleteLetter = () => {
   if (currentTile > 0) {
     currentTile--;
@@ -121,46 +117,50 @@ const deleteLetter = () => {
   }
 };
 
-// For checking the answer 'ENTER'
 const checkRow = () => {
   const guess = guessRows[currentRow].join("");
-
   if (currentTile > 4) {
-    console.log(guess, wordle);
-    flipTile();
-    if (wordle == guess) {
-      giveMessage("YEP");
-      isGameOver = true;
-      return;
-    } else {
-      if (currentRow >= 5) {
-        isGameOver = false;
-        giveMessage("NOPE");
-        return;
-      }
-      if (currentRow < 5) {
-        currentRow++;
-        currentTile = 0;
-      }
-    }
+    fetch(`http://localhost:8000/check/?word=${guess}`)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json == "Entry word not found") {
+          showMessage("word not in list");
+          return;
+        } else {
+          flipTile();
+          if (wordle == guess) {
+            showMessage("Magnificent!");
+            isGameOver = true;
+            return;
+          } else {
+            if (currentRow >= 5) {
+              isGameOver = true;
+              showMessage("Game Over");
+              return;
+            }
+            if (currentRow < 5) {
+              currentRow++;
+              currentTile = 0;
+            }
+          }
+        }
+      })
+      .catch((err) => console.log(err));
   }
 };
 
-// For displaying message
-const giveMessage = (message) => {
+const showMessage = (message) => {
   const messageElement = document.createElement("p");
   messageElement.textContent = message;
   messageDisplay.append(messageElement);
-  setTimeout(() => messageDisplay.removeChild(messageElement), 3000);
+  setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
 };
 
-// For adding color to keyboard
 const addColorToKey = (keyLetter, color) => {
   const key = document.getElementById(keyLetter);
   key.classList.add(color);
 };
 
-// For flipping and colors in tiles
 const flipTile = () => {
   const rowTiles = document.querySelector("#guessRow-" + currentRow).childNodes;
   let checkWordle = wordle;
